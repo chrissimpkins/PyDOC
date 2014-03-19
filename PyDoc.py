fd156f5b60387b87efba2f2a86e5ec8cffff159d
Chris <git.simpkins@gmail.com>
2014-03-19 19:37:44 -0400

    updated status and error messages

--- PyDoc.py
+++ PyDoc.py
@@ -15,7 +15,7 @@ class PythonThreeDocCommand(sublime_plugin.TextCommand):
             if selections:
                 needle = self.view.substr(selections[0])
                 if len(needle) == 0:
-                    print("PyDOC Plugin Error: There was no selected string for the Python 3 documentation search.  Please select a string in your document and try again.")
+                    print("PyDOC Plugin Error: You did not select text for the Python 3 documentation search.  Please select a Python built-in object in your editor and try again.")
                 else:
                     if version_info[0] == 2:
                         needle = urllib.quote_plus(needle) # url encode Python 2 interpreter
@@ -23,12 +23,16 @@ class PythonThreeDocCommand(sublime_plugin.TextCommand):
                     else:
                         url = "http://docs.python.org/3/search.html?q=" + needle + "&check_keywords=yes"
                         url = urllib.parse.urlparse(url).geturl() # url encode Python 3 interpreter
-                    print("PyDOC: Performing search for Python 3 documentation on the keyword, '" + needle + "'")
+                    user_message = "PyDOC: Performing search for Python 3 documentation on the keyword, '" + needle + "'"
+                    print(user_message)
+                    sublime.status_message(user_message)
                     webbrowser.open(url)
             else:
-                print("PyDOC Plugin Error: There was no selected string for the search.  Please select a string in your document and try again.")
+                print("PyDOC Plugin Error: You did not select text for the Python 3 documentation search.  Please select a Python built-in object in your editor and try again.")
+                sublime.status_message("PyDOC Plugin Error: Text was not selected")
         except Exception as e:
             print("PyDOC Plugin Error: There was an error during the execution of the plugin.\n")
+            sublime.status_message("PyDOC Plugin Error: Open console for info")
             raise e
 
 # Python 2 Documentation Search
@@ -39,7 +43,7 @@ class PythonTwoDocCommand(sublime_plugin.TextCommand):
             if selections:
                 needle = self.view.substr(selections[0])
                 if len(needle) == 0:
-                    print("PyDOC Plugin Error: There was no selected string for the Python 2 documentation search.  Please select a string in your document and try again.")
+                    print("PyDOC Plugin Error: You did not select text for the Python 2 documentation search.  Please select a Python built-in object in your editor and try again.")
                 else:
                     if version_info[0] == 2:
                         needle = urllib.quote_plus(needle)
@@ -47,11 +51,15 @@ class PythonTwoDocCommand(sublime_plugin.TextCommand):
                     else:
                         url = "http://docs.python.org/2/search.html?q=" + needle + "&check_keywords=yes"
                         url = urllib.parse.urlparse(url).geturl() # url encode Python 3 interpreter
-                    print("PyDOC: Performing search for Python 2 documentation on the keyword, '" + needle + "'")
+                    user_message = "PyDOC: Performing search for Python 2 documentation on the keyword, '" + needle + "'"
+                    print(user_message)
+                    sublime.status_message(user_message)
                     webbrowser.open(url)
             else:
-                print("PyDOC Plugin Error: There was no selected string for the search.  Please select a string in your document and try again.")
+                print("PyDOC Plugin Error: You did not select text for the Python 2 documentation search.  Please select a Python built-in object in your editor and try again.")
+                sublime.status_message("PyDOC Plugin Error: Text was not selected")
         except Exception as e:
             print("PyDOC Plugin Error: There was an error during the execution of the plugin.\n")
+            sublime.status_message("PyDOC Plugin Error: Open console for info")
             raise e
 
